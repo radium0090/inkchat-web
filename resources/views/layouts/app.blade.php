825a7ce6e8913499049a4f32c9e1b1840b13e746
皮皮 <pp@pipideMacBook-Pro.local>
2019-11-27 18:22:35 +0900

    change google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,17 @@
     <link href="{{ asset('css/tagsinput.css') }}" rel="stylesheet" />
 
     <script data-ad-client="ca-pub-1668811522927993" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153523067-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-153523067-1');
+    </script>
+
     
 </head>
 <body>
